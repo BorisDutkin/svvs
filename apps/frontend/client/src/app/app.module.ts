be0920580b6049ea80c593031bcc033338a68243
Boris Dutkin <boris.webdev@gmail.com>
2021-05-10 07:36:13 +0300

    feat(scss): set imports of the app
    Global and local SCSS settings
    
    #12

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MatButtonModule, MatIconModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
