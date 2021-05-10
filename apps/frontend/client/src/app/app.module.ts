1c911215bc29c57346dac1c28d81008ecf7401c1
Boris Dutkin <boris.webdev@gmail.com>
2021-05-10 08:31:20 +0300

    feat(scss): final stages
    setting libs / apps / globals
    
    Closes #12

--- apps/frontend/client/src/app/app.module.ts
+++ apps/frontend/client/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
+import { UiComponentModule } from '@svvs/ui-component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatButtonModule, MatIconModule, MatCardModule],
+  imports: [BrowserModule, UiComponentModule],
   providers: [],
   bootstrap: [AppComponent],
 })
